4199a633d610dad800e5c79f5625a3e2745d00a2
AznanSeyam <106177845+AznanSeyam@users.noreply.github.com>
2026-03-17 00:00:14 +0600

    Remove unused RegisterDto import from auth.controller.ts

--- bank-management-system-backend/src/auth/auth.controller.ts
+++ bank-management-system-backend/src/auth/auth.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
