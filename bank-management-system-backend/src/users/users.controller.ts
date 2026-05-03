a44455924df86bfcce2bdd78e127d7a2cffecb26
AznanSeyam <106177845+AznanSeyam@users.noreply.github.com>
2026-05-03 22:31:29 +0600

    Update users.controller.ts

--- bank-management-system-backend/src/users/users.controller.ts
+++ bank-management-system-backend/src/users/users.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Post, Body, Get, Param, Put, Delete, BadRequestException } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
